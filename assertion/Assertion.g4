34a83166d1468d3dadbdb73d00e01091aae531be
Pham Hong Long <longph1989@gmail.com>
2020-06-07 16:32:23 +0800

    update

--- assertion/Assertion.g4
+++ assertion/Assertion.g4
@@ -21,20 +21,20 @@ term
       :   TRUE
       |   func op num
       |   func op func
-      |   VAR op num                        // bounds for whole input
-      |   VAR op array                      // bounds for input segments
+//      |   VAR op num                        // bounds for whole input
+//      |   VAR op array                      // bounds for input segments
       |   VAR LB INT RB op num              // bounds for each element
       |   VAR LB INT RB op VAR LB INT RB    // fairness
       ;
 
 func
-      :   'd0' LP VAR CM VAR RP           // 0-norm distance function
-      |   'd2' LP VAR CM VAR RP           // 2-norm distance function
-      |   'di' LP VAR CM VAR RP           // inf-norm distance function
-      |   'arg_max' LP VAR RP             // arg max index for output
-      |   'arg_min' LP VAR RP             // arg min index for output
-      |   'lin_inp' LP VAR CM array RP    // linear application for input
-      |   'lin_out' LP VAR CM array RP    // linear application for output
+      :   D0 LP VAR CM VAR RP           // 0-norm distance function
+      |   D2 LP VAR CM VAR RP           // 2-norm distance function
+      |   DI LP VAR CM VAR RP           // inf-norm distance function
+      |   ARG_MAX LP VAR RP             // arg max index for output
+      |   ARG_MIN LP VAR RP             // arg min index for output
+      |   LIN_INP LP VAR CM array RP    // linear application for input
+      |   LIN_OUT LP VAR CM array RP    // linear application for output
       ;
 
 op
@@ -55,6 +55,16 @@ num
       |   FLT
       ;
 
+
+TRUE  :   'True' ;
+
+D0    :   'd0' ;
+D2    :   'd2' ;
+DI    :   'di' ;
+ARG_MAX : 'arg_max' ;
+ARG_MIN : 'arg_min' ;
+LIN_INP : 'lin_inp' ;
+LIN_OUT : 'lin_out' ;
 
 VAR   :   [a-zA-Z_][a-zA-Z0-9_]* ;
 INT   :   '0' | '-'? [1-9][0-9]*;
@@ -78,7 +88,5 @@ NE    :   '!=' ;
 OR    :   '\\/' ;
 AND   :   '/\\' ;
 IMP   :   '=>' ;
-
-TRUE  :   'True';
 
 WS    :   [ \t\r\n]+ -> skip ;
--- assertion/AssertionVisitor.py
+++ assertion/AssertionVisitor.py
@@ -5,7 +5,11 @@ if __name__ is not None and "." in __name__:
 else:
     from AssertionParser import AssertionParser
 
+import ast
+
 from assertion import *
+from lib_functions import *
+from functools import partial
 
 # This class defines a complete generic visitor for a parse tree produced by AssertionParser.
 
@@ -48,9 +52,87 @@ class AssertionVisitor(ParseTreeVisitor):
 
     def visitTerm(self, ctx:AssertionParser.TermContext):
         if ctx.TRUE():
-            return Term('True', None, None)
+            return TrueTerm()
         else:
-            return Term('False', None, None)
+            if ctx.func(0):
+                lhs = self.visitFunc(ctx.func(0))
+
+                if ctx.func(1):
+                    rhs = self.visitFunc(ctx.func(1))
+                elif ctx.num():
+                    rhs = ast.literal_eval(ctx.num().getText())
+            elif ctx.VAR(0):
+                vars1 = list()
+                vars1.append(Var(ctx.VAR(0).getText()))
+
+                idx1 = ast.literal_eval(ctx.INT(0).getText())
+                lhs = Function(partial(index, idx1), vars1)
+
+                if ctx.VAR(1):
+                    vars2 = list()
+                    vars2.append(Var(ctx.VAR(1).getText()))
+
+                    idx2 = ast.literal_eval(ctx.INT(1).getText())
+                    rhs = Function(partial(index, idx2), vars2)
+                elif ctx.num():
+                    rhs = ast.literal_eval(ctx.num().getText())
+
+            op = self.visitOp(ctx.op())
+
+            return GeneralTerm(op, lhs, rhs)
+
+    def visitFunc(self, ctx:AssertionParser.FuncContext):
+        vars = list()
+
+        if ctx.D0():
+            vars.append(Var(ctx.VAR(0).getText()))
+            vars.append(Var(ctx.VAR(1).getText()))
+            return Function(d0, vars)
+
+        elif ctx.D2():
+            vars.append(Var(ctx.VAR(0).getText()))
+            vars.append(Var(ctx.VAR(1).getText()))
+            return Function(d2, vars)
+
+        elif ctx.DI():
+            vars.append(Var(ctx.VAR(0).getText()))
+            vars.append(Var(ctx.VAR(1).getText()))
+            return Function(di, vars)
+
+        elif ctx.ARG_MAX():
+            vars.append(Var(ctx.VAR(0).getText()))
+            return Function(arg_max, vars)
+
+        elif ctx.ARG_MIN():
+            vars.append(Var(ctx.VAR(0).getText()))
+            return Function(arg_min, vars)
+
+        elif ctx.LIN_INP():
+            vars.append(Var(ctx.VAR(0).getText()))
+            array = np.array(ast.literal_eval(ctx.array().getText()))
+            return Function(partial(lin_inp, array), vars)
+
+        elif ctx.LIN_OUT():
+            vars.append(Var(ctx.VAR(0).getText()))
+            array = np.array(ast.literal_eval(ctx.array().getText()))
+            return Function(partial(lin_out, array), vars)
+            
+        else:
+            return None
+
+    def visitOp(self, ctx:AssertionParser.OpContext):
+        if ctx.GE():
+            return Op.GE
+        elif ctx.GT():
+            return Op.GT
+        elif ctx.LE():
+            return Op.LE
+        elif ctx.LT():
+            return Op.LT
+        elif ctx.EQ():
+            return Op.EQ
+        elif ctx.NE():
+            return Op.NE
 
 
 del AssertionParser
