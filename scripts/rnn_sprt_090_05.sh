76a317ec1200fdc2b5d3979979a43b24baac3d9e
Pham Hong Long <longph1989@gmail.com>
2020-07-10 15:11:55 +0800

    remove redundant

--- scripts/rnn_sprt_090_05.sh
+++
@@ -1,7 +0,0 @@
-mkdir results_sprt_090
-mkdir results_sprt_090/rnn_05
-
-(time python -u source/run_rnn.py --spec benchmark/rnn/nnet/jigsaw_gru/spec.json --algorithm sprt --threshold 0.90 --eps 0.5 --dataset jigsaw) &> results_sprt_090/rnn_05/log_jigsaw_gru.txt
-(time python -u source/run_rnn.py --spec benchmark/rnn/nnet/jigsaw_lstm/spec.json --algorithm sprt --threshold 0.90 --eps 0.5 --dataset jigsaw) &> results_sprt_090/rnn_05/log_jigsaw_lstm.txt
-(time python -u source/run_rnn.py --spec benchmark/rnn/nnet/wiki_gru/spec.json --algorithm sprt --threshold 0.90 --eps 0.5 --dataset wiki) &> results_sprt_090/rnn_05/log_wiki_gru.txt
-(time python -u source/run_rnn.py --spec benchmark/rnn/nnet/wiki_lstm/spec.json --algorithm sprt --threshold 0.90 --eps 0.5 --dataset wiki) &> results_sprt_090/rnn_05/log_wiki_lstm.txt
