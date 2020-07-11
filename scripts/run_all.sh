05117beb758ce4c49fa821a0b98ac571eb9dc05a
Pham Hong Long <longph1989@gmail.com>
2020-07-11 10:52:46 +0800

    recover scripts

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,21 +1,21 @@
-# ./scripts/reluplex_opti.sh
+./scripts/reluplex_opti.sh
 ./scripts/eran_opti_01.sh
-# ./scripts/fairness_opti.sh
-# ./scripts/rnn_opti_01.sh
+./scripts/fairness_opti.sh
+./scripts/rnn_opti_01.sh
 
-# ./scripts/reluplex_sprt_090.sh
+./scripts/reluplex_sprt_090.sh
 ./scripts/eran_sprt_090_01.sh
-# ./scripts/fairness_sprt_090.sh
-# ./scripts/rnn_sprt_090_01.sh
+./scripts/fairness_sprt_090.sh
+./scripts/rnn_sprt_090_01.sh
 
-# ./scripts/reluplex_sprt_095.sh
+./scripts/reluplex_sprt_095.sh
 ./scripts/eran_sprt_095_01.sh
-# ./scripts/fairness_sprt_095.sh
-# ./scripts/rnn_sprt_095_01.sh
+./scripts/fairness_sprt_095.sh
+./scripts/rnn_sprt_095_01.sh
 
-# ./scripts/reluplex_sprt_099.sh
+./scripts/reluplex_sprt_099.sh
 ./scripts/eran_sprt_099_01.sh
-# ./scripts/fairness_sprt_099.sh
-# ./scripts/rnn_sprt_099_01.sh
+./scripts/fairness_sprt_099.sh
+./scripts/rnn_sprt_099_01.sh
 
 echo DONE
