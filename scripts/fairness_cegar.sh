1457bf25a06d16b3e7396bbe28418e4eabbf75de
Pham Hong Long <longph1989@gmail.com>
2021-05-08 22:20:50 +0800

    minor

--- scripts/fairness_cegar.sh
+++ scripts/fairness_cegar.sh
@@ -1,6 +1,11 @@
-mkdir results_cegar
-mkdir results_cegar/fairness
+mkdir results_fairness
 
-(time python -u source/run_fairness.py --spec benchmark/fairness/bank/spec.json --algorithm deepcegar --dataset bank --has_ref --max_ref 100) &> results_cegar/fairness/log_bank.txt
-(time python -u source/run_fairness.py --spec benchmark/fairness/census/spec.json --algorithm deepcegar --dataset census --has_ref --max_ref 100) &> results_cegar/fairness/log_census.txt
-(time python -u source/run_fairness.py --spec benchmark/fairness/credit/spec.json --algorithm deepcegar --dataset credit --has_ref --max_ref 100) &> results_cegar/fairness/log_credit.txt
+(time python -u source/run_fairness.py --spec benchmark/fairness/bank/spec.json --algorithm deepcegar --dataset bank --has_ref --max_ref 100) &> results_fairness/log_bank.txt
+(time python -u source/run_fairness.py --spec benchmark/fairness/census/spec.json --algorithm deepcegar --dataset census --has_ref --max_ref 100) &> results_fairness/log_census.txt
+(time python -u source/run_fairness.py --spec benchmark/fairness/credit/spec.json --algorithm deepcegar --dataset credit --has_ref --max_ref 100) &> results_fairness/log_credit.txt
+
+mkdir results_fairness_noref
+
+(time python -u source/run_fairness.py --spec benchmark/fairness/bank/spec.json --algorithm deepcegar --dataset bank) &> results_fairness_noref/log_bank.txt
+(time python -u source/run_fairness.py --spec benchmark/fairness/census/spec.json --algorithm deepcegar --dataset census) &> results_fairness_noref/log_census.txt
+(time python -u source/run_fairness.py --spec benchmark/fairness/credit/spec.json --algorithm deepcegar --dataset credit) &> results_fairness_noref/log_credit.txt
