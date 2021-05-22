494a88fedaf7849bcec82c1418e9cc8a9adffed5
Pham Hong Long <longph1989@gmail.com>
2021-05-22 10:22:33 +0800

    more scripts

---
+++ scripts/run_backdoor.sh
@@ -0,0 +1,7 @@
+./scripts/run_backdoor_attack_only.sh
+./scripts/run_backdoor_fix_pos.sh
+./scripts/run_backdoor_not_fix_pos.sh
+./scripts/run_backdoor_fix_pos_005.sh
+./scripts/run_backdoor_not_fix_pos_005.sh
+./scripts/run_backdoor_fix_pos_010.sh
+./scripts/run_backdoor_not_fix_pos_010.sh
