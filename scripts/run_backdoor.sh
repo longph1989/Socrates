2cfd58c241f11f145b1f794891bce59d179cdc98
Pham Hong Long <longph1989@gmail.com>
2021-06-04 09:02:12 +0800

    minor

--- scripts/run_backdoor.sh
+++ scripts/run_backdoor.sh
@@ -1,13 +1,14 @@
-./scripts/run_backdoor_attack_only.sh
+# ./scripts/run_backdoor_attack_only.sh
+
+./scripts/run_backdoor_fix_pos.sh
+./scripts/run_backdoor_not_fix_pos.sh
 
 ./scripts/run_backdoor_fix_pos_i010.sh
 ./scripts/run_backdoor_fix_pos_s055.sh
 ./scripts/run_backdoor_fix_pos_t005.sh
-./scripts/run_backdoor_fix_pos.sh
 
 ./scripts/run_backdoor_not_fix_pos_i010.sh
 ./scripts/run_backdoor_not_fix_pos_s055.sh
 ./scripts/run_backdoor_not_fix_pos_t005.sh
-./scripts/run_backdoor_not_fix_pos.sh
 
 echo DONE
