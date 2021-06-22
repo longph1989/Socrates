814767ebf81263b715a23677a264be10b6c1c542
Pham Hong Long <longph1989@gmail.com>
2021-06-23 00:51:21 +0800

    minor

--- scripts/run_backdoor_hyptest1.sh
+++ scripts/run_backdoor_hyptest1.sh
@@ -10,7 +10,7 @@ echo Running mnist_sigmoid_5_50.tf
 (timeout 600m time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_sigmoid_5_50/spec.json --algorithm backdoor --num_imgs 5 --rate 0.9 --dataset mnist_fc) &> results_backdoor_hyptest1/log_mnist_sigmoid_5_50.tf.txt
 echo Running mnist_tanh_3_10.tf
 (timeout 120m time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_tanh_3_10/spec.json --algorithm backdoor --num_imgs 5 --rate 0.9 --dataset mnist_fc) &> results_backdoor_hyptest1/log_mnist_tanh_3_10.tf.txt
-# echo Running mnist_tanh_5_50.tf
-# (timeout 600m time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_tanh_5_50/spec.json --algorithm backdoor --num_imgs 5 --rate 0.9 --dataset mnist_fc) &> results_backdoor_hyptest1/log_mnist_tanh_5_50.tf.txt
+echo Running mnist_tanh_5_50.tf
+(timeout 600m time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_tanh_5_50/spec.json --algorithm backdoor --num_imgs 5 --rate 0.9 --dataset mnist_fc) &> results_backdoor_hyptest1/log_mnist_tanh_5_50.tf.txt
 
 echo Done! > results_backdoor_hyptest1/done.txt
