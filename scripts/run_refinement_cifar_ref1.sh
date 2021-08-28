a0561755a37e1e11d337e21620a4ea42d39de9eb
Pham Hong Long <longph1989@gmail.com>
2021-08-28 10:31:59 +0800

    cifar ref 1 and 2

---
+++ scripts/run_refinement_cifar_ref1.sh
@@ -0,0 +1,12 @@
+mkdir results_refinement_cifar_ref1
+
+echo Running cifar_relu_6_100.tf
+(time python -u source/run_refinement.py --spec benchmark/cegar/nnet/cifar_relu_6_100/spec.json --algorithm refinement --has_ref --ref_typ 1 --max_ref 100 --dataset cifar_fc) &> results_refinement_cifar_ref1/log_cifar_relu_6_100.tf.txt
+
+echo Running cifar_relu_7_1024.tf
+(time python -u source/run_refinement.py --spec benchmark/cegar/nnet/cifar_relu_7_1024/spec.json --algorithm refinement --has_ref --ref_typ 1 --max_ref 100 --dataset cifar_fc --num_tests 10) &> results_refinement_cifar_ref1/log_cifar_relu_7_1024.tf.txt
+
+echo Running cifar_relu_9_200.tf
+(time python -u source/run_refinement.py --spec benchmark/cegar/nnet/cifar_relu_9_200/spec.json --algorithm refinement --has_ref --ref_typ 1 --max_ref 100 --dataset cifar_fc) &> results_refinement_cifar_ref1/log_cifar_relu_9_200.tf.txt
+
+echo Done!
