b0ef3abf612d65031c38fdf87693ebb555e1132a
Pham Hong Long <longph1989@gmail.com>
2021-10-14 10:14:49 +0800

    minor scripts backdoor

--- scripts/run_backdoor_verifyI1.sh
+++ scripts/run_backdoor_verifyI1.sh
@@ -92,22 +92,22 @@ echo Running mnist_tanh_5_50.tf
 (time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_tanh_5_50/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc) &> results_backdoor_verifyI1/log_mnist_tanh_5_50.tf.txt
 
 echo Running mnist_relu_4_1024.tf
-(time python -u source/run_backdoor.py --spec benchmark/eran/nnet/mnist_relu_4_1024/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc --num_procs 5) &> results_backdoor_verifyI1/log_mnist_relu_4_1024.tf.txt
+(time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_relu_4_1024/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc --num_procs 5) &> results_backdoor_verifyI1/log_mnist_relu_4_1024.tf.txt
 echo Running mnist_relu_6_100.tf
-(time python -u source/run_backdoor.py --spec benchmark/eran/nnet/mnist_relu_6_100/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc) &> results_backdoor_verifyI1/log_mnist_relu_6_100.tf.txt
+(time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_relu_6_100/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc) &> results_backdoor_verifyI1/log_mnist_relu_6_100.tf.txt
 echo Running mnist_relu_9_200.tf
-(time python -u source/run_backdoor.py --spec benchmark/eran/nnet/mnist_relu_9_200/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc) &> results_backdoor_verifyI1/log_mnist_relu_9_200.tf.txt
+(time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_relu_9_200/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_fc) &> results_backdoor_verifyI1/log_mnist_relu_9_200.tf.txt
 
 echo Running mnist_conv_small_relu.tf
-(time python -u source/run_backdoor.py --spec benchmark/backdoor/eran/nnet/mnist_conv_small_relu/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_conv) &> results_backdoor_verifyI1/log_mnist_conv_small_relu.tf.txt
+(time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_conv_small_relu/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_conv) &> results_backdoor_verifyI1/log_mnist_conv_small_relu.tf.txt
 echo Running mnist_conv_small_relu_diffai.tf
-(time python -u source/run_backdoor.py --spec benchmark/backdoor/eran/nnet/mnist_conv_small_relu_diffai/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_conv) &> results_backdoor_verifyI1/log_mnist_conv_small_relu_diffai.tf.txt
+(time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_conv_small_relu_diffai/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_conv) &> results_backdoor_verifyI1/log_mnist_conv_small_relu_diffai.tf.txt
 echo Running mnist_conv_small_relu_pgd.tf
-(time python -u source/run_backdoor.py --spec benchmark/backdoor/eran/nnet/mnist_conv_small_relu_pgd/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_conv) &> results_backdoor_verifyI1/log_mnist_conv_small_relu_pgd.tf.txt
+(time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/mnist_conv_small_relu_pgd/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset mnist_conv) &> results_backdoor_verifyI1/log_mnist_conv_small_relu_pgd.tf.txt
 
 # echo Running cifar_relu_6_100.tf
-# (time python -u source/run_backdoor.py --spec benchmark/eran/nnet/cifar_relu_6_100/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset cifar_fc) &> results_backdoor_verifyI1/log_cifar_relu_6_100.tf.txt
+# (time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/cifar_relu_6_100/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset cifar_fc) &> results_backdoor_verifyI1/log_cifar_relu_6_100.tf.txt
 # echo Running cifar_relu_9_200.tf
-# (time python -u source/run_backdoor.py --spec benchmark/eran/nnet/cifar_relu_9_200/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset cifar_fc) &> results_backdoor_verifyI1/log_cifar_relu_9_200.tf.txt
+# (time python -u source/run_backdoor.py --spec benchmark/cegar/nnet/cifar_relu_9_200/spec.json --algorithm backdoor --total_imgs 100 --rate 1.0 --dataset cifar_fc) &> results_backdoor_verifyI1/log_cifar_relu_9_200.tf.txt
 
 echo Done! > results_backdoor_verifyI1/done.txt
