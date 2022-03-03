fd65fa36302a7aeaac3b4c983726d2a6828388a6
Pham Hong Long <longph1989@gmail.com>
2022-03-03 10:27:14 +0800

    gen samples

--- source/solver/continual_impl.py
+++ source/solver/continual_impl.py
@@ -1,4 +1,18 @@
+from utils import *
+
 
 class ContinualImpl():
     def solve(self, model, assertion, display=None):
+        lower, upper = model.lower, model.upper
+
+        print("lower = {}".format(lower))
+        print("upper = {}".format(upper))
+
+        for i in range(1000):
+            x = generate_x(len(lower), lower, upper)
+            y = model.apply(x).reshape(-1)
+
+            print("x = {}".format(x))
+            print("y = {}".format(y))
+
         return None
