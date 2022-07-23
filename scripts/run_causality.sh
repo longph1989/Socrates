fe9946a6362362325ce7d13851336dfd2087779f
Pham Hong Long <longph1989@gmail.com>
2022-07-23 15:30:00 +0800

    minor script causality

--- scripts/run_causality.sh
+++ scripts/run_causality.sh
@@ -3,12 +3,14 @@
 # requirements follow socrates: https://github.com/longph1989/Socrates
 # tested with python3.7
 
-#time python source/run_causal.py --spec benchmark/causal/credit/spec_age.json --algorithm causal --dataset credit
-#time python source/run_causal.py --spec benchmark/causal/credit/spec_gender.json --algorithm causal --dataset credit
+mkdir -p results_causality
 
-time python source/run_causal.py --spec benchmark/causal/bank/spec_age.json --algorithm causal --dataset bank
+time python source/run_causal.py --spec benchmark/causal/credit/spec_age.json --algorithm causal --dataset credit > results_causality/credit_age.txt
+time python source/run_causal.py --spec benchmark/causal/credit/spec_gender.json --algorithm causal --dataset credit > results_causality/credit_gender.txt
 
-#time python source/run_causal.py --spec benchmark/causal/census/spec_race.json --algorithm causal --dataset census
-#time python source/run_causal.py --spec benchmark/causal/census/spec_age.json --algorithm causal --dataset census
-#time python source/run_causal.py --spec benchmark/causal/census/spec_gender.json --algorithm causal --dataset census
+time python source/run_causal.py --spec benchmark/causal/bank/spec_age.json --algorithm causal --dataset bank > results_causality/bank_age.txt
+
+time python source/run_causal.py --spec benchmark/causal/census/spec_race.json --algorithm causal --dataset census > results_causality/census_race.txt
+time python source/run_causal.py --spec benchmark/causal/census/spec_age.json --algorithm causal --dataset census > results_causality/census_age.txt
+time python source/run_causal.py --spec benchmark/causal/census/spec_gender.json --algorithm causal --dataset census > results_causality/census_gender.txt
 
