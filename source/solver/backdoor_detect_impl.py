5ba28af2ef27b6b31dcf17112e9a8c6448bd9bad
Pham Hong Long <longph1989@gmail.com>
2023-02-01 15:53:24 +0800

    update resnet34

--- source/solver/backdoor_detect_impl.py
+++ source/solver/backdoor_detect_impl.py
@@ -361,7 +361,165 @@ class SubResNet(nn.Module):
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
+##################################################
 
+################################################## Resnet - 34 https://github.com/bboylyg/NAD
+
+def conv3x3RN34(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+class BasicBlockRN34(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlockRN34, self).__init__()
+        self.conv1 = conv3x3RN34(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3RN34(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class BottleneckRN34(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BottleneckRN34, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet34(nn.Module):
+
+    def __init__(self, depth, num_classes=10, block_name='BasicBlock'):
+        super(ResNet34, self).__init__()
+        # Model type specifies number of layers for CIFAR-10 model
+        if block_name.lower() == 'basicblock':
+            assert (depth - 2) % 6 == 0, 'When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202'
+            n = (depth - 2) // 6
+            block = BasicBlockRN34
+        elif block_name.lower() == 'bottleneck':
+            assert (depth - 2) % 9 == 0, 'When use bottleneck, depth should be 9n+2, e.g. 20, 29, 47, 56, 110, 1199'
+            n = (depth - 2) // 9
+            block = BottleneckRN34
+        else:
+            raise ValueError('block_name shoule be Basicblock or Bottleneck')
+
+        self.inplanes = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, n)
+        self.layer2 = self._make_layer(block, 32, n, stride=2)
+        self.layer3 = self._make_layer(block, 64, n, stride=2)
+        self.avgpool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)  # 32x32
+
+        x = self.layer1(x)  # 32x32
+        x = self.layer2(x)  # 16x16
+        x = self.layer3(x)  # 8x8
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+class SubResNet34(nn.Module):
+    def __init__(
+        self, block = BasicBlockRN34, num_classes=10,
+    ):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+        
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x    
 
 ##################################################
 
@@ -474,13 +632,6 @@ class BackdoorDetectImpl():
 
         dict_params = dict(sub_params)
 
-        # for name, param in params:
-        #     if dataset == 'mnist' and ('fc4.weight' in name or 'fc4.bias' in name):
-        #         dict_params[name].data.copy_(param.data)
-        #     elif (dataset == 'cifar10_inv' or dataset == 'cifar10_sem' or dataset == 'cifar10_tro') \
-        #         and ('fc3.weight' in name or 'fc3.bias' in name):
-        #         dict_params[name].data.copy_(param.data)
-
         for name, param in params:
             if dataset == 'mnist' and ('fc4.weight' in name or 'fc4.bias' in name):
                 dict_params[name].data.copy_(param.data)
@@ -586,7 +737,7 @@ class BackdoorDetectImpl():
 
             size_input, size_last = (3, 32, 32), 512
 
-        elif dataset == 'cifar10_18' or dataset == 'cifar10_34' or dataset == 'cifar10_50':
+        elif dataset == 'cifar10_18' or dataset == 'cifar10_50':
 
             def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
                 model = ResNet(block, layers, **kwargs)
@@ -598,6 +749,17 @@ class BackdoorDetectImpl():
                         state_dict = torch.load(file_name, map_location=device)
                         model.load_state_dict(state_dict)
                 return model
+        
+        ######################## ResNet34 - NAD
+        elif dataset == 'cifar10_34':
+            def __resnet(pretrained, depth, num_classes):
+                model = ResNet34(depth, num_classes)
+                if pretrained:
+                    if file_name.endswith('tar'):
+                        checkpoint = torch.load(file_name, map_location='cpu')
+                        model.load_state_dict(checkpoint['state_dict'])
+                return model
+        #######################
 
             last_layer = 'avgpool'
 
@@ -606,23 +768,25 @@ class BackdoorDetectImpl():
                 model = _resnet("resnet18", BasicBlockRN, [2, 2, 2, 2], pretrained=True, progress=True, device="cpu")
                 model.avgpool.register_forward_hook(get_activation(last_layer))
                 sub_model = SubResNet(BasicBlockRN)
+                size_input, size_last = (3, 32, 32), 512 # 64 for RN34, 512 for RN18
             
             elif dataset == 'cifar10_34':
                 file_name = './backdoor_models/cifar10_resnet34_bd.pth.tar'
-                model = _resnet("resnet34", BasicBlockRN, [3, 4, 6, 3], pretrained=True, progress=True, device="cpu")
+                model = __resnet(pretrained=True, depth=32, num_classes=10)
                 model.avgpool.register_forward_hook(get_activation(last_layer))
-                sub_model = SubResNet(BasicBlockRN)
+                sub_model = SubResNet34(block=BasicBlockRN34)
+                size_input, size_last = (3, 32, 32), 64 # 64 for RN34, 512 for RN18
             
             elif dataset == 'cifar10_50':
                 file_name = './backdoor_models/cifar10_wrn_bd.pth.tar'
                 model = _resnet("resnet50", Bottleneck, [3, 4, 6, 3], pretrained=True, progress=True, device="cpu")
                 model.avgpool.register_forward_hook(get_activation(last_layer))
                 sub_model = SubResNet(Bottleneck)
+                size_input, size_last = (3, 32, 32), 512 # 64 for RN34, 512 for RN18
                 
             train_dataset = datasets.CIFAR10('./data', train=True, download=True, transform=transform)
             test_dataset = datasets.CIFAR10('./data', train=False, transform=transform)
 
-            size_input, size_last = (3, 32, 32), 512
         
         elif dataset == 'WRN-16-1' or dataset == 'WRN-16-2' or dataset == 'WRN-40-1' or \
             dataset == 'WRN-40-2' or dataset == 'WRN-10-1' or dataset == 'WRN-10-2':
